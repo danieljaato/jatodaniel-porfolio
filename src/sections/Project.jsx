4c4d60e157cf3481547f1f1f48d0a1daa5354bb5
JatoDaniel <jatodaniel944@gmail.com>
2026-09-21 16:38:39 -0700

    Update site code

--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -11,7 +11,7 @@ const projects = [
     image: m1,
     tags: ["React", "Typescript", ],
     link: "https://prestige-spa.vercel.app/",
-    github: "#",
+    github: "https://github.com/danieljaato",
   },
 
   
@@ -23,7 +23,7 @@ const projects = [
     image: m1,
     tags: ["React", "Typescript", "NodeJS"],
     link: "#",
-    github: "#",
+    github: "https://github.com/danieljaato",
   },
 
    {
@@ -33,7 +33,7 @@ const projects = [
     image: da,
     tags: ["React", "Typescript"],
     link: "https://jat-autos.vercel.app/",
-    github: "#",
+    github: "https://github.com/danieljaato",
   },
 
    {
@@ -43,7 +43,7 @@ const projects = [
     image: de,
     tags: ["React", "Typescript", "NodeJS"],
     link: "https://pet-hub-lake.vercel.app/",
-    github: "#",
+    github: "https://github.com/danieljaato",
   },
 ];
 
